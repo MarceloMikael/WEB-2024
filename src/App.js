0501bb887d64a430b4d9dd8b421c919c712560b8
MarceloMikael <marcelo.mplp@hotmail.com>
2024-03-05 20:33:18 -0300

    atv00

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import MeusDados from './components/ativadade00/01MeusDados';
+import MeusDados2 from './components/ativadade00/02MeusDados';
+import Temperatura from './components/ativadade00/03Temperatura';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MeusDados/>
+      <hr />
+      <MeusDados2 nome="Marcelo & Elpidio" curso="Engenharia De Software" universidade="UFC"/>
+      <hr />
+      <Temperatura c="40" f="70" k="200" />
     </div>
   );
 }
